ddfb93cc7b586dc9019cfca976dd286ed76ff863
--replace-all <al.gadzhalov@gmail.com>
2022-12-24 11:26:55 +0200

    Changed: fetch fron Osmosis working

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import { createProtobufRpcClient, QueryClient } from "@cosmjs/stargate";
+import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
+import { ServiceClientImpl } from "./../proto/out/cosmos/base/tendermint/v1beta1/query";
+
+const fetchFromOsmosis = async () => {
+    const tendermintClient = await Tendermint34Client.connect("https://rpc.osmosis.zone:443");
+    const queryClient = new QueryClient(tendermintClient);
+    const rpcClient = createProtobufRpcClient(queryClient);
+    const queryService = new ServiceClientImpl(rpcClient);    
+    const queryResult = await queryService.GetLatestBlock({});
+    console.log(queryResult);
+}
+
+fetchFromOsmosis();
