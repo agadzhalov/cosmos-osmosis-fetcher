1ca3504f3d60fd197d9d038ea87262ef761ae407
--replace-all <al.gadzhalov@gmail.com>
2022-12-24 11:32:50 +0200

    Added: generated proto codec files

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { createProtobufRpcClient, QueryClient } from "@cosmjs/stargate";
 import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
-import { ServiceClientImpl } from "./../proto/out/cosmos/base/tendermint/v1beta1/query";
+import { ServiceClientImpl } from "./../proto/dest/cosmos/base/tendermint/v1beta1/query";
 
 const fetchFromOsmosis = async () => {
     const tendermintClient = await Tendermint34Client.connect("https://rpc.osmosis.zone:443");
