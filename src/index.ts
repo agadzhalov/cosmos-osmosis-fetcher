f578a9a0bc29bd207e3a2cd10407d6c1f7464288
--replace-all <al.gadzhalov@gmail.com>
2022-12-27 16:11:55 +0200

    Added: more comments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import App from "./app";
 
-const app = new App("https://rpc-test.osmosis.zone:443");
+/**
+ * Used as a starting point of the application
+ * @param endpoint is a gRPC endpoint
+ */
+
+const endpoint = "https://rpc-test.osmosis.zone:443";
+
+const app = new App(endpoint);
 app.run();
